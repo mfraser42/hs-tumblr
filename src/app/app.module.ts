e586cfd5117e656e33c4a1b948ee0ae2ca828680
Michael Fraser <mfraser702@gmail.com>
2018-09-30 11:38:39 -0700

    some basic templating, basic post component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    FavoritesComponent,
+    PostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
